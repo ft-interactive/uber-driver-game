26dd893b54ee508fa15bf81692b1a4928dde588a
David Blood <davidbjourno@users.noreply.github.com>
2017-06-12 17:20:51 +0100

    Initial commit: uses starter kit (commit b6c3e2b)

---
+++ client/index.js
@@ -0,0 +1,19 @@
+/*
+  TODO: delete this comment
+
+  This file is where you bootstrap your JS code
+  For example import stuff here:
+
+  import {select} from 'd3-selection';
+  import myComponent from './components/my-component';
+
+  Split logical parts of you project into components e.g.
+
+  /client
+    - /components
+        - /component-name
+            - styles.scss
+            - index.js
+            - template.html
+
+*/
