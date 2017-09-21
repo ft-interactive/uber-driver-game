fa61962c34fd3cfebb483ec9de05f1a99b6b661e
Callum Locke <callum.locke@gmail.com>
2017-09-21 13:39:10 +0100

    add new starter-kit project

---
+++ client/index.js
@@ -0,0 +1,29 @@
+import './styles.scss';
+
+/*
+  TODO: delete this comment
+
+  This file is where you bootstrap your JS code
+  For example import stuff here:
+
+  import {select} from 'd3-selection';
+  import myComponent from './components/my-component';
+
+  Split logical parts of you project into components e.g.
+
+  /client
+    - /components
+        - /component-name
+            - styles.scss
+            - index.js
+            - template.html
+
+  If you want to import some data, just import it like normal:
+
+  import myData from './data.csv';
+
+  `myData` will be a string that you can then parse into an object using, for example, d3.csvParse()
+
+  You can import CSV, TSV, TXT, XML and JSON this way! Note, however, that it will increase your
+  bundle size, which may increase the time to first render in some cases!
+*/
---
+++ webpack.config.babel.js
@@ -0,0 +1,139 @@
+import 'babel-polyfill';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import ImageminWebpackPlugin from 'imagemin-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+// import { HotModuleReplacementPlugin } from 'webpack';
+import { resolve } from 'path';
+import getContext from './config';
+import * as nunjucksFilters from './views/filters';
+
+module.exports = async (env = 'development') => ({
+  entry: {
+    bundle: ['babel-polyfill', './client/index.js'],
+  },
+  resolve: {
+    modules: ['node_modules', 'bower_components'],
+  },
+  output: {
+    filename: env === 'production' ? '[name].[hash].js' : '[name].js',
+    path: resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(txt|csv|tsv|xml)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'raw-loader',
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                'env',
+                {
+                  // Via: https://docs.google.com/document/d/1mByh6sT8zI4XRyPKqWVsC2jUfXHZvhshS5SlHErWjXU/view
+                  browsers: ['last 2 versions', 'ie >= 11', 'safari >= 10', 'ios >= 9'],
+                },
+              ],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images/',
+              name: '[name]--[hash].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name]--[hash].[ext]',
+            },
+          },
+          {
+            loader: 'extract-loader',
+          },
+          { loader: 'css-loader', options: { sourceMap: true, url: true } },
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+        ],
+      },
+      {
+        test: /\.(html|njk)$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: [
+                'img:src',
+                'link:href',
+              ],
+              root: resolve(__dirname, 'client'),
+            },
+          },
+          {
+            loader: 'nunjucks-html-loader',
+            options: {
+              searchPaths: [
+                resolve(__dirname, 'views'),
+              ],
+              filters: nunjucksFilters,
+              context: await getContext(),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            { loader: 'css-loader', options: { sourceMap: true } },
+            { loader: 'postcss-loader', options: { sourceMap: true } },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+                includePaths: ['bower_components'],
+              },
+            },
+          ],
+          fallback: 'style-loader',
+        }),
+      },
+    ],
+  },
+  devServer: {
+    hot: false, // Needed for live-reloading Nunjucks templates.
+    allowedHosts: [
+      '.ngrok.io',
+    ],
+  },
+  devtool: 'source-map',
+  plugins: [
+    // new HotModuleReplacementPlugin(), // Re-enable if devServer.hot is set to true
+    new ExtractTextPlugin({
+      filename: env === 'production' ? '[name].[contenthash].css' : '[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'client/index.html',
+    }),
+    env === 'production'
+      ? new ImageminWebpackPlugin({ test: /\.(jpe?g|png|gif|svg)$/i })
+      : undefined,
+  ].filter(i => i),
+});
