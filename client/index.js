c737cfcf14e84ce10b25c41268e876a0b58e08f1
Robin Kwong <robin.kwong@ft.com>
2017-10-01 17:56:09 +0100

    update background image when rendering choices
    For some reason, tags for the current knot show up here. Previously, it
    grabbed tags from the previous knot.
    
    This seems to fix the off-by-one not syncing up problem, although i
    don’t know why this works

--- client/index.js
+++ client/index.js
@@ -129,6 +129,7 @@ function continueStory() {
 
   console.log(`rideCountTotal: ${rideCountTotal}, ridesObj: ${ridesObj}`);
 
+
   function showPanel() {
     const panelIn = anime.timeline();
 
@@ -408,13 +409,7 @@ function continueStory() {
 
   // Generate story text - loop through available content
   while (story.canContinue) {
-    // Update background image if appropriate
-    const bgImageURL = stateUtils.getBackgroundImageURL();
-    if (bgImageURL) {
-      gameContainer.setBackgroundImage(bgImageURL);
-    }
-
-
+ 
     const existingChoicesContainer = knotElement.querySelector('.choices-container');
     // Get ink to generate the next paragraph
     const paragraphText = story.Continue();
@@ -444,6 +439,13 @@ function continueStory() {
     const choiceElement = document.createElement('button');
     choiceElement.classList.add('choice');
     choiceElement.innerHTML = `<span>${choice.text}</span>`;
+    console.log('tags: '+story.currentTags);
+
+    // Update background image if appropriate
+    const bgImageURL = stateUtils.getBackgroundImageURL();
+    if (bgImageURL) {
+      gameContainer.setBackgroundImage(bgImageURL);
+    }
 
     // Make it look different if there's more than one choice available
     if (story.currentTags.indexOf('button') === -1) {
