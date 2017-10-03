5aa6bfdb1938e18839a197cfd9696c899560ece2
Joanna Kao <joanna.kao@ft.com>
2017-10-02 20:11:38 -0400

    turn on function to send events to ga

--- client/components/analytics/index.js
+++ client/components/analytics/index.js
@@ -5,13 +5,13 @@ module.exports = (eventCategory, eventAction, eventLabel, eventValue) => {
     console.log('>>>>> ga event: ', { eventCategory, eventAction, eventLabel, eventValue });
     return;
   }
-  //
-  // ga('send', {
-  //   hitType: 'event',
-  //   eventCategory,
-  //   eventAction,
-  //   eventLabel,
-  //   eventValue,
-  //   transport: 'beacon',
-  // });
+
+  ga('send', {
+    hitType: 'event',
+    eventCategory,
+    eventAction,
+    eventLabel,
+    eventValue,
+    transport: 'beacon',
+  });
 };
